fa182135bbd669866570d2f0e77b1f631606fccd
micuat <micuat@gmail.com>
2017-08-25 21:43:49 -0400

    write header for trac

--- osceleton_piecemeta/osceleton_piecemeta.py
+++ osceleton_piecemeta/osceleton_piecemeta.py
@@ -21,30 +21,24 @@ with open(filename, newline='') as csvfile:
 #      print('/'.join(row))
 print(joints)
 
-
-with open('pm-' + filename, 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile, delimiter='\t',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    head = []
-    head.append('timestamp')
-    count = 0
-    for timestamp in joints:
-      for data in joints[timestamp]:
-        head.append(str(count) + '/x')
-        head.append(str(count) + '/y')
-        head.append(str(count) + '/z')
-        count += 1
-      csvwriter.writerow(head)
-      break
-    
-    count = 1
-    for timestamp in joints:
-      row = []
-      row.append(count)
-      row.append(timestamp)
-      for data in joints[timestamp]:
-        row.append(data['x'])
-        row.append(data['y'])
-        row.append(data['z'])
-      csvwriter.writerow(row)
-      count += 1
+newfilename = 'pm-' + filename
+with open(newfilename, 'w', newline='') as csvfile:
+  csvfile.write('''PathFileType	4	(X/Y/Z)	%s
+DataRate	CameraRate	NumFrames	NumMarkers	Units	OrigDataRate	OrigDataStartFrame	OrigNumFrames
+30.00	30.00	      %d	24	mm	30.00	1	      %d
+Frame#	Time	Head			SpineShoulder			SpineMid			SpineBase			CollarRight			ShoulderRight			ElbowRight			WristRight			HandRight			FingerRight			CollarLeft			ShoulderLeft			ElbowLeft			WristLeft			HandLeft			FingerLeft			HipRight			KneeRight			AnkleRight			FootRight			HipLeft			KneeLeft			AnkleLeft			FootLeft
+		X1	Y1	Z1	X2	Y2	Z2	X3	Y3	Z3	X4	Y4	Z4	X5	Y5	Z5	X6	Y6	Z6	X7	Y7	Z7	X8	Y8	Z8	X9	Y9	Z9	X10	Y10	Z10	X11	Y11	Z11	X12	Y12	Z12	X13	Y13	Z13	X14	Y14	Z14	X15	Y15	Z15	X16	Y16	Z16	X17	Y17	Z17	X18	Y18	Z18	X19	Y19	Z19	X20	Y20	Z20	X21	Y21	Z21	X22	Y22	Z22	X23	Y23	Z23	X24	Y24	Z24\n'''
+    % (newfilename, len(joints), len(joints)))
+  csvwriter = csv.writer(csvfile, delimiter='\t',
+                          quotechar='|', quoting=csv.QUOTE_MINIMAL)
+  count = 1
+  for timestamp in joints:
+    row = []
+    row.append(count)
+    row.append(timestamp)
+    for data in joints[timestamp]:
+      row.append(data['x'])
+      row.append(data['y'])
+      row.append(data['z'])
+    csvwriter.writerow(row)
+    count += 1
